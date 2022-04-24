bd9825eefdf2e3cef5ff39a1a50e7e74f6b6f54b
paulschick <paulschick19@gmail.com>
2022-04-24 18:08:58 -0400

    Adds java jdk to initial installations, adds vimrc

---
+++ dotfiles/.vimrc
@@ -0,0 +1,30 @@
+let mapleader = " "
+
+:set ignorecase
+:set hlsearch
+:set mouse=a
+:set smartcase
+:set smartindent
+:set splitbelow
+:set splitright
+:set expandtab
+:set tabstop=4
+:set shiftwidth=4
+:set nu
+:set rnu
+:set wrap
+:set background=dark
+:set autochdir
+
+nnoremap <Leader>s :source ~/.vimrc<CR>
+inoremap jk <Esc>
+nnoremap <Leader>e :Lexplore<CR>
+nnoremap <Leader>v <C-w>v
+nnoremap <Leader>h <C-w>s
+nnoremap <C-h> <C-w>h
+nnoremap <C-j> <C-w>j
+nnoremap <C-k> <C-w>k
+nnoremap <C-l> <C-w>l
+nnoremap <Tab> :bnext<CR>
+nnoremap <S-Tab> :bprev<CR>
+nnoremap <Leader><Space> :noh<CR>
--- post-install/functions/initial_installs.sh
+++ post-install/functions/initial_installs.sh
@@ -17,18 +17,20 @@ function first_installs {
     echo_message info "Setting git email to $GIT_EMAIL"
     git config --global user.email $GIT_EMAIL
     echo_message success "Successfully installed and configured git"
-    # check_install_apt_package build-essential
-    # check_install_apt_package make
-    # check_install_apt_package python3-dev
-    # check_install_apt_package python3-pip
-    # check_install_apt_package python3-venv
-    # check_install_apt_package checkinstall
-    # check_install_apt_package sqlite3
-    # check_install_apt_package zsh
-    # check_install_apt_package nettools
+    check_install_apt_package build-essential
+    check_install_apt_package make
+    check_install_apt_package python3-dev
+    check_install_apt_package python3-pip
+    check_install_apt_package python3-venv
+    check_install_apt_package checkinstall
+    check_install_apt_package sqlite3
+    check_install_apt_package zsh
+    check_install_apt_package nettools
+    # Install default Java jre
+    check_install_apt_package default-jre
     
     # this is for TreeSitter-nvim
-    # check_install_apt_package g++
+    check_install_apt_package g++
 
     check_install_apt_package vim
 }
